ba24582c8270f81c01481f063853c0a2d8f3d8c8
LeoRiether <leonardoalves8217@gmail.com>
2021-06-01 17:31:05 -0300

    Output json to stdout

--- src/main.py
+++ src/main.py
@@ -1,19 +1,25 @@
+import sys
+import json
 import config.credentials as creds
 from extract import extract
 import api
 
 contest_url = 'https://codeforces.com/group/nituVTsHQX/contest/329742'
 
+def log(*x):
+    print(*x, file=sys.stderr)
+
 def main():
-    print(f"Hello {creds.handle}! Logging you in...")
+    log(f"Hello {creds.handle}! Logging you in...")
 
     with api.logged_in_session(creds.handle, creds.password) as s:
-        print("Logged in!")
+        log("Logged in!")
 
-        result = extract(s, contest_url)
-        print(result)
+        result = extract(s, contest_url, log)
 
-        print("Done!")
+        print(json.dumps(result))
+
+        log("Done!")
 
 
 if __name__ == '__main__':
